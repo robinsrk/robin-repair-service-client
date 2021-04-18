e5d711924466f127c850a150a786b70d984914ff
robinsrk <robinsrk3@gmail.com>
2021-04-18 23:33:45 +0600

    update footer

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { MDBFooter, MDBContainer, MDBRow, MDBCol } from "mdbreact";
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdbreact";
 
 const Footer = () => {
   return (
@@ -9,38 +9,50 @@ const Footer = () => {
         <MDBContainer fluid className="text-center text-md-left">
           <MDBRow>
             <MDBCol md="4">
-              <h5 className="title">Footer Content</h5>
+              <h5 className="title">Robin-Computer-Service</h5>
               <p>
-                Here you can use rows and columns here to organize your footer
-                content.
+                Welcome to Robin's computer repair service for all kinds of
+                computer repairs and diagnostic{" "}
               </p>
             </MDBCol>
             <MDBCol md="4">
-              <h5 className="title">Links</h5>
+              <h5 className="title">Contact us</h5>
               <ul>
                 <li className="list-unstyled">
-                  <a href="#!">Link 1</a>
+                  <a href="#!">About</a>
                 </li>
                 <li className="list-unstyled">
-                  <a href="#!">Link 2</a>
+                  <a href="#!">Details</a>
                 </li>
                 <li className="list-unstyled">
-                  <a href="#!">Link 3</a>
-                </li>
-                <li className="list-unstyled">
-                  <a href="#!">Link 4</a>
+                  <a href="#!">Address</a>
                 </li>
               </ul>
             </MDBCol>
             <MDBCol md="4">
-              <h3>Hello world</h3>
+              <h3>Find us</h3>
+              <ul>
+                <li className="list-unstyled">
+                  <a className="m-1" href="http://www.facebook.com">
+                    <MDBIcon fab icon="facebook-f"></MDBIcon>
+                  </a>
+                  <a className="m-1" href="http://www.youtube.com">
+                    <MDBIcon fab icon="youtube"></MDBIcon>
+                  </a>
+                  <a className="m-1" href="http://www.facebook.com">
+                    <MDBIcon fab icon="twitter"></MDBIcon>
+                  </a>
+                </li>
+              </ul>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
         <div className="footer-copyright text-center py-3">
           <MDBContainer fluid>
-            &copy; {new Date().getFullYear()} Copyright:{" "}
-            <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+            &copy; {new Date().getFullYear()} Copyright:
+            <a href="www.robin-computer-service.web.app">
+              robin-computer-service
+            </a>
           </MDBContainer>
         </div>
       </MDBFooter>
